28353879da346411e5cec7ce29f0a58d1d59a6b5
Towsif Ahmed Omi <towsif031@gmail.com>
2021-12-31 20:31:45 +0600

    working on js

---
+++ assets/js/Ball.js
@@ -0,0 +1,7 @@
+export default class Ball {
+	constructor(ballElem) {
+		this.ballElem = ballElem;
+	}
+
+	update(delta) {}
+}
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,16 @@
-console.log('js loaded');
+import Ball from './Ball.js';
+
+const ball = new Ball(document.querySelector('#ball'));
+
+let lastTime;
+function update(time) {
+	if (lastTime !== null) {
+		const delta = time - lastTime;
+		ball.update(delta);
+	}
+
+	lastTime = time;
+	window.requestAnimationFrame(update);
+}
+
+window.requestAnimationFrame(update);
