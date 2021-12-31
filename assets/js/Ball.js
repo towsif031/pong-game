bd5a960790dfb1950719ffcfe72895b3df8e0eab
Towsif Ahmed Omi <towsif031@gmail.com>
2021-12-31 21:27:01 +0600

    set x, y positions with js.

--- assets/js/Ball.js
+++ assets/js/Ball.js
@@ -13,7 +13,18 @@ export default class Ball {
 		this.ballElem.style.setProperty('--x', value);
 	}
 
+	get y() {
+		return parseFloat(
+			getComputedStyle(this.ballElem).getPropertyValue('--y')
+		);
+	}
+
+	set y(value) {
+		this.ballElem.style.setProperty('--y', value);
+	}
+
 	update(delta) {
 		this.x = 5;
+		this.y = 15;
 	}
 }
