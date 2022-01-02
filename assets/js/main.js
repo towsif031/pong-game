82e1b429151df0c83df37ae3f62b31bce0377106
Towsif Ahmed Omi <towsif031@gmail.com>
2022-01-02 20:50:40 +0600

    changing the background color with time.

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,6 +14,12 @@ function update(time) {
 		ball.update(delta, [playerPaddle.rect(), computerPaddle.rect()]);
 		computerPaddle.update(delta, ball.y);
 
+		const hue = parseFloat(
+			getComputedStyle(document.documentElement).getPropertyValue('--hue')
+		);
+
+		document.documentElement.style.setProperty('--hue', hue + delta * 0.01);
+
 		if (isLose()) handleLose();
 	}
 
